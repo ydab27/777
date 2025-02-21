555f1c7b92ba7819fe9fe5007467a52c968dd7ee
ydab27 <andruhadvydab27@gmail.com>
2025-02-21 13:36:49 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# Получаем значения переменных окружения
+api_id = os.getenv("TELEGRAM_API_ID")  # Переменная окружения для API ID
+api_hash = os.getenv("TELEGRAM_API_HASH")  # Переменная окружения для API Hash
