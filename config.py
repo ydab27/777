96b9f714dd0ac4d8de400eab061b8e23e614942a
ydab27 <andruhadvydab27@gmail.com>
2025-02-21 22:41:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 # Получаем значения переменных окружения
 API_ID = os.getenv("TELEGRAM_API_ID")  # Переменная окружения для API ID
 API_HASH = os.getenv("TELEGRAM_API_HASH")  # Переменная окружения для API Hash
+PHONE = os.getenv("TELEGRAM_PHONE")
